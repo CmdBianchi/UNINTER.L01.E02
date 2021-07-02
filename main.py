bd04802824debf8a4b8aa33095af6cc497d4fa28
marcus bianchi <marcusbianchi92@hotmail.com>
2021-07-02 11:40:01 -0300

    Commit 02

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #--------------------------------------------------------------------#
 #------- > Exercicio 02 Lista 01
-print('-'*20)
-nome = input('Digite o seu primeiro nome: ')
-sobrenome = input('Digite seu sobrenome: ')
+print('-'*30)
+name = input('Digite o seu primeiro nome: ')
+surname = input('Digite seu sobrenome: ')
 ru = input('Digite o seu RU: ')
 
 def EmailGenerator(nome, sobrenome, ru):
@@ -10,7 +10,7 @@ def EmailGenerator(nome, sobrenome, ru):
     _gerador = gerador[0][0] + sobrenome + ru[-2]+ru[-1]
     return  _gerador
 
-print('-'*20)
-print(EmailGenerator(nome, sobrenome, ru)+"@algoritmos.com.br")
+print('-'*30)
+print(EmailGenerator(name, surname, ru)+"@algoritmos.com.br")
 
 
